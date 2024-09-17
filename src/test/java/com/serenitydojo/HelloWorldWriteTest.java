d1d5b0613c80bac6b74bb71fef390a3222b8962b
NZQA_WGTN\RithyM <rithy.mech@nzqa.govt.nz>
2024-09-18 07:43:18 +1200

    exercise 1

---
+++ src/test/java/com/serenitydojo/HelloWorldWriteTest.java
@@ -0,0 +1,12 @@
+package com.serenitydojo;
+
+import org.junit.Test;
+
+public class HelloWorldWriteTest {
+
+    @Test
+    public void shouldWriteHelloWorldToTheConsole() {
+        System.out.println("Hello World!");
+    }
+
+}
